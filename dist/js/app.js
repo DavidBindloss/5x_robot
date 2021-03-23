094fc56ca8582fbd69384f1c7d7b362917bc45f7
David <d.bindloss@gmail.com>
2021-03-23 16:24:49 +1100

    Made the robot a home

---
+++ dist/js/app.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // initialize the robot board.
+    initializeRobotBoard();
+});
+
+var WIDTH = 5;
+var HEIGHT = 5;
+
+var initializeRobotBoard = function() {
+    var board = document.getElementById("board");
+
+}
+
